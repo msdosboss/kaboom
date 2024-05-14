2cc8c8608327b72ac1244e9a609e41043ae5658a
msdosboss <jacobt0722@gmail.com>
2024-05-14 11:50:44 -0700

    added some engineering items

--- kaboom.lua
+++ kaboom.lua
@@ -10,7 +10,33 @@ function f:COMBAT_LOG_EVENT(event, ...)
 		if subEvent == "SPELL_DAMAGE" then
 			PlaySoundFile("Interface\\AddOns\\kaboom\\kaboom.ogg", "Master")
 		end
-	end		
+	elseif spellName == "Thorium Grenade" then
+		PlaySoundFile("Interface\\AddOns\\kaboom\\kaboom.ogg", "Master")
+
+	elseif spellName == "Rough Dynamite" then
+		PlaySoundFile("Interface\\AddOns\\kaboom\\kaboom.ogg", "Master")
+
+	elseif spellName == "Rough Copper Bomb" then
+		PlaySoundFile("Interface\\AddOns\\kaboom\\kaboom.ogg", "Master")
+
+	elseif spellName == "Coarse Dynamite" then
+		PlaySoundFile("Interface\\AddOns\\kaboom\\kaboom.ogg", "Master")
+
+	elseif spellName == "Heavy Dynamite" then
+		PlaySoundFile("Interface\\AddOns\\kaboom\\kaboom.ogg", "Master")
+	
+	elseif spellName == "Large Copper Bomb" then
+		PlaySoundFile("Interface\\AddOns\\kaboom\\kaboom.ogg", "Master")
+	
+	elseif spellName == "Small Bronze Bomb" then
+		PlaySoundFile("Interface\\AddOns\\kaboom\\kaboom.ogg", "Master")
+	
+	elseif spellName == "Thorium Grenade" then
+		PlaySoundFile("Interface\\AddOns\\kaboom\\kaboom.ogg", "Master")
+	end
+
+
+				
 end
 
 function f:PLAYER_LOGIN(self, event, ...)
